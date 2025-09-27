20579fde184b1c59ff1ed83d4a2dfaac1456a81f
Layse Breder <ysepbreder@gmail.com>
2025-09-27 13:28:31 -0300

    Alteracoes no layout do tasks

--- src/app/(app)/tasks/_layout.tsx
+++ src/app/(app)/tasks/_layout.tsx
@@ -1,10 +1,14 @@
+// src/app/tasks/_layout.tsx
 import { Stack } from 'expo-router';
+import { TaskProvider } from '@/src/context/TaskContext';
 
 export default function TasksLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: 'Tarefas' }} />
-      <Stack.Screen name="[id]" options={{ title: 'Editar tarefa' }} />
-    </Stack>
+    <TaskProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ title: 'Tarefas' }} />
+        <Stack.Screen name="[id]" options={{ title: 'Editar tarefa' }} />
+      </Stack>
+    </TaskProvider>
   );
 }
